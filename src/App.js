c7f425a8efe112e8d4d4ccc792651550038d85f9
mdabuzafar <mdabuzafar78@gmail.com>
2025-10-29 14:33:42 +0530

    refactor: update the project structure

--- src/App.js
+++ src/App.js
@@ -1,55 +1,9 @@
-import { GoBell, GoDatabase } from 'react-icons/go';
-import Button from './Button';
+import ButtonPage from "./pages/ButtonPage";
+
 const App = () => {
   return (
     <div>
-      <div>
-        <Button className='mb-5'>
-          <GoBell /> Test 1
-        </Button>
-      </div>
-      <div>
-        <Button primary className='mb-10'>
-          <GoDatabase />
-          Test 1
-        </Button>
-      </div>
-      <div>
-        <Button primary rounded outline>
-          Test 1
-        </Button>
-      </div>
-      <div>
-        <Button secondary rounded outline>
-          Test 1
-        </Button>
-      </div>
-      <div>
-        <Button success rounded outline>
-          Test 1
-        </Button>
-      </div>
-      <div>
-        <Button danger outline>
-          Test 2
-        </Button>
-      </div>
-      <div>
-        <Button warning>Test 3</Button>
-      </div>
-      <div>
-        <Button success>Test 4</Button>
-      </div>
-      <div>
-        <Button success outline>
-          Test 5
-        </Button>
-      </div>
-      <div>
-        <Button danger rounded>
-          Test 6
-        </Button>
-      </div>
+      <ButtonPage />
     </div>
   );
 };
